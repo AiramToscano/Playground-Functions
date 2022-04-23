39b843147b7e8c65c9d7b70a13baee4a0110cbc9
Airam Toscano <airamlobato@gmail.com>
2022-04-23 19:21:52 -0400

    modificando algumas coisas

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,9 +14,28 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+
+  const hydrate = (string) => {
+    let vetor = /\d+/g;
+    
+    const result = string.match(vetor);
+    let novovetor = [];
+    soma = 0;
+    
+    
+      for( let i = 0; i < result.length ; i += 1) {
+              let convertido = parseInt(result[i])
+              novovetor.push(convertido);
+              soma += novovetor[i];
+     }
+      if ( soma > 1) {
+        return `${soma} copos de água`;
+      }
+      else {
+        return `${soma} copo de água`;
+      }
+     
+    }
 
 module.exports = {
   generatePhoneNumber,
